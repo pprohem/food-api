74591332474db80976639fd7c66feb18b78b09cd
pprohem <pprg00@gmail.com>
2023-02-14 12:31:24 -0300

    FIXED ERROR CORSCONFIG

--- src/main/java/com/foodcommerce/foodapi/config/CorsConfig.java
+++ src/main/java/com/foodcommerce/foodapi/config/CorsConfig.java
@@ -14,11 +14,14 @@ public class CorsConfig {
 
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*")
+        registry.addMapping("/**")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
-            .allowedHeaders("")
+            .allowedHeaders("*")
             .allowedOrigins("*");
       }
     };
   }
+
+
+  
 }
---
+++ src/main/java/com/foodcommerce/foodapi/config/CorsFilterConfig.java
@@ -0,0 +1,53 @@
+package com.foodcommerce.foodapi.config;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilterConfig implements Filter {
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, x-auth-token");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if (!(request.getMethod().equalsIgnoreCase("OPTIONS"))) {
+            try {
+                chain.doFilter(req, res);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            System.out.println("Pre-flight");
+            response.setHeader("Access-Control-Allowed-Methods", "POST, PUT, GET, DELETE");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "authorization, content-type,x-auth-token, " +
+                    "access-control-request-headers, access-control-request-method, accept, origin, authorization, x-requested-with");
+
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+
+    }
+
+    public void init(FilterConfig filterConfig) {
+    }
+
+    public void destroy() {
+    }
+
+}
